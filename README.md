583fd02d015c90577a3f4f6df7bdf60956a838c5
TheHong <chcheung.office@gmail.com>
2020-04-08 11:06:51 -0400

    Indentation errors and small bugs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cGAN
 
-Based on https://machinelearningmastery.com/how-to-develop-a-pix2pix-gan-for-image-to-image-translation/
+Based on https://machinelearningmastery.com/how-to-develop-a-pix2pix-gan-for-image-to-image-translation/.
+
+Paper is: https://arxiv.org/pdf/1611.07004.pdf
--- load.py
+++ load.py
@@ -13,18 +13,18 @@ from matplotlib import pyplot
 
 # load all images in a directory into memory
 def load_images(path, size=(256,512)):
-	src_list, tar_list = list(), list()
-	# enumerate filenames in directory, assume all are images
-	for filename in listdir(path):
-		# load and resize the image
-		pixels = load_img(path + filename, target_size=size)
-		# convert to numpy array
-		pixels = img_to_array(pixels)
-		# split into satellite and map
-		sat_img, map_img = pixels[:, :256], pixels[:, 256:]
-		src_list.append(sat_img)
-		tar_list.append(map_img)
-	return [asarray(src_list), asarray(tar_list)]
+    src_list, tar_list = list(), list()
+    # enumerate filenames in directory, assume all are images
+    for filename in listdir(path):
+        # load and resize the image
+        pixels = load_img(path + filename, target_size=size)
+        # convert to numpy array
+        pixels = img_to_array(pixels)
+        # split into satellite and map
+        sat_img, map_img = pixels[:, :256], pixels[:, 256:]
+        src_list.append(sat_img)
+        tar_list.append(map_img)
+    return [asarray(src_list), asarray(tar_list)]
 
 
 # load and prepare training images
@@ -33,14 +33,14 @@ def load_real_samples(filename):
     return a list of two NumPy arrays: 
     the first for source images and the second for corresponding target images.
     """
-	# load compressed arrays
-	data = load(filename)
-	# unpack arrays
-	X1, X2 = data['arr_0'], data['arr_1']
-	# scale from [0,255] to [-1,1]
-	X1 = (X1 - 127.5) / 127.5
-	X2 = (X2 - 127.5) / 127.5
-	return [X1, X2]
+    # load compressed arrays
+    data = load(filename)
+    # unpack arrays
+    X1, X2 = data['arr_0'], data['arr_1']
+    # scale from [0,255] to [-1,1]
+    X1 = (X1 - 127.5) / 127.5
+    X2 = (X2 - 127.5) / 127.5
+    return [X1, X2]
 
 
 def generate_real_samples(dataset, n_samples, patch_shape):
@@ -50,23 +50,23 @@ def generate_real_samples(dataset, n_samples, patch_shape):
     and the corresponding discriminator label of class=1 to indicate they are real.
 
     """
-	# unpack dataset
-	trainA, trainB = dataset
-	# choose random instances
-	ix = randint(0, trainA.shape[0], n_samples)
-	# retrieve selected images
-	X1, X2 = trainA[ix], trainB[ix]
-	# generate 'real' class labels (1)
-	y = ones((n_samples, patch_shape, patch_shape, 1))
-	return [X1, X2], y
+    # unpack dataset
+    trainA, trainB = dataset
+    # choose random instances
+    ix = randint(0, trainA.shape[0], n_samples)
+    # retrieve selected images
+    X1, X2 = trainA[ix], trainB[ix]
+    # generate 'real' class labels (1)
+    y = ones((n_samples, patch_shape, patch_shape, 1))
+    return [X1, X2], y
 
 
 def generate_fake_samples(g_model, samples, patch_shape):
-	# generate fake instance
-	X = g_model.predict(samples)
-	# create 'fake' class labels (0)
-	y = zeros((len(X), patch_shape, patch_shape, 1))
-	return X, y
+    # generate fake instance
+    X = g_model.predict(samples)
+    # create 'fake' class labels (0)
+    y = zeros((len(X), patch_shape, patch_shape, 1))
+    return X, y
 
 
 if __name__ == "__main__":
